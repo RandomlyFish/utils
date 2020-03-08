bb2afdd827d12c5f13822cf6780f9c044577ffd5
RandomlyFish <Tobias.Rheindorf@gmail.com>
2020-03-08 05:29:11 +0100

    Added getIncluded and getBetween, containsAny > includesAny

--- StringUtil.js
+++ StringUtil.js
@@ -1,26 +1,57 @@
-module.exports = {
-    /**
-     * Checks if a string contains any of the provided strings
-     * @param {string} string 
-     * @param {string[]} strings 
-     */
-    containsAny: (string = "", strings = []) => {
-        for (let i = 0; i < strings.length; i++) {
-            if (string.indexOf(strings[i]) >= 0) {
-                return true;
-            }
+/**
+ * Checks if a string contains any of a string
+ * @param {string} string - The string to use as a source
+ * @param {string[]} strings - Array of strings to check
+ */
+module.exports.includesAny = (string, strings) => {
+    for (let i = 0; i < strings.length; i++) {
+        if (string.indexOf(strings[i]) >= 0) {
+            return true;
         }
-
-        return false;
-    },
-
-    /** 
-     * Replaces all occurances of a string inside a string with another
-     * @param {string} string - The string to use as a source
-     * @param {string} find - The string to replace
-     * @param {string} replaceWith - The string which will replace the found strings
-     */
-    replace: (string, find, replaceWith) => {
-        return string.split(find).join(replaceWith);
     }
-};
+
+    return false;
+}
+
+/**
+ * Returns any strings that are included in another string
+ * @param {string} string - The string to use as a source
+ * @param {string[]} strings - Array of strings to check
+ */
+module.exports.getIncluded = (string, strings) => {
+    const included = [];
+    for (let i = 0; i < strings.length; i++) {
+        if (string.indexOf(strings[i]) >= 0) {
+            included.push(strings[i]);
+        }
+    }
+
+    return included;
+}
+
+/** 
+ * Replaces all occurrences of a string inside a string with another
+ * @param {string} string - The string to use as a source
+ * @param {string} find - The string to replace
+ * @param {string} replaceWith - The string which will replace the found strings
+ */
+module.exports.replace = (string, find, replaceWith) => {
+    return string.split(find).join(replaceWith);
+}
+
+/** 
+ * Returns a string between two strings
+ * @param {string} string - The string to use as a source
+ * @param {string} after - Returns the string after this
+ * @param {string} upto - Returns the string upto this
+ */
+module.exports.getBetween = (string, after, upto) => {
+    if (string.indexOf(after) >= 0) {
+        string = string.substring(string.indexOf(after) + after.length);
+    }
+    if (string.indexOf(upto) >= 0) {
+        string = string.substring(0, string.indexOf(upto));
+    }
+
+    return string;
+}
--- test.js
+++ test.js
@@ -9,22 +9,55 @@ function logSuccess(msg) {
     console.log("[32m" + msg + "[39m");
 }
 
-/** @param {string} name @param {*} input @param {*} expectedOutput */
-function test(name, input, expectedOutput) {
-    if (input !== expectedOutput) {
-        logError(`Test: ${name} failed, input: "${input}" did not return the expected output: "${expectedOutput}"`);
+/** @param {string} name @param {*} input @param {*} expectedValue */
+function test(name, input, expectedValue) {
+    let matchingTypes = typeof input === typeof expectedValue;
+
+    if (typeof input === "string") {
+        input = '"' + input + '"';
+    }
+    if (typeof expectedValue === "string") {
+        expectedValue = '"' + expectedValue + '"';
+    }
+
+    if (Array.isArray(input) === true) {
+        input = "[" + input.toString() + "]";
+    }
+    if (Array.isArray(expectedValue) === true) {
+        expectedValue = "[" + expectedValue.toString() + "]";
+    }
+
+    if (input !== expectedValue || matchingTypes === false) {
+        logError(`Test: ${name} failed, input: ${input} does not match the expected value: ${expectedValue}`);
         process.exit();
     } else {
         console.log(`Test: ${name} passed`);
     }
 }
 
-test("Contains none", StringUtil.containsAny("Test string", []), false);
-test("Contains none 2", StringUtil.containsAny("Test string", ["find"]), false);
-test("Contains single", StringUtil.containsAny("Test string", ["string"]), true);
+test("Includes any none", StringUtil.includesAny("Test string", []), false);
+test("Includes any none 2", StringUtil.includesAny("Test string", ["find"]), false);
+test("Includes any single", StringUtil.includesAny("Test string", ["string"]), true);
+
+test("Get included none", StringUtil.getIncluded("Test string", []), []);
+test("Get included none 2", StringUtil.getIncluded("Test string", ["notString"]), []);
+test("Get included none 3", StringUtil.getIncluded("Test string", ["Test string extended"]), []);
+test("Get included single", StringUtil.getIncluded("Test string", ["string"]), ["string"]);
+test("Get included single 2", StringUtil.getIncluded("Test string string string", ["string"]), ["string"]);
+test("Get included multiple", StringUtil.getIncluded("Test string", ["Test", "string"]), ["Test", "string"]);
+test("Get included multiple 2", StringUtil.getIncluded("Test string", ["Test", "string", "notString"]), ["Test", "string"]);
 
 test("Replace none", StringUtil.replace("Test string 1", "123", "321"), "Test string 1");
 test("Replace single", StringUtil.replace("Test string", "Test", "Working"), "Working string");
 test("Replace multiple", StringUtil.replace("Test 123 test 123", "123", "abc"), "Test abc test abc");
+
+test("Get between none", StringUtil.getBetween("Start string end", "notStart ", " notEnd"), "Start string end");
+test("Get between middle", StringUtil.getBetween("Start string end", "Start ", " end"), "string");
+test("Get between only start", StringUtil.getBetween("Start string end", "Start ", " notEnd"), "string end");
+test("Get between only end", StringUtil.getBetween("Start string end", "notStart ", " end"), "Start string");
+test("Get between middle multiple", StringUtil.getBetween("Start Start string end end", "Start ", " end"), "Start string");
+test("Get between start multiple", StringUtil.getBetween("Start Start string end", "Start ", " notEnd"), "Start string end");
+test("Get between end multiple", StringUtil.getBetween("Start string end end", "Start ", " end"), "string");
+test("Get between end in beginning", StringUtil.getBetween(" end", "Start ", " end"), "");
 
 logSuccess(`All tests passed`);
